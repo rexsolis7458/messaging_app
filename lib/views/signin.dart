d7112db9e2a940c055efb58fc9e2f9bc0e9d326f
Caramelite <carmelatrujillo17@gmail.com>
2021-04-23 15:00:11 +0800

    Added some folders and files. Made some changes in main.dart

---
+++ lib/views/signin.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:messaging_app/widget/widget.dart';
+
+class SignIn extends StatefulWidget {
+  SignIn({Key key}) : super(key: key);
+
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: appBarMain(context));
+  }
+}
---
+++ lib/widget/widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Widget appBarMain(BuildContext context) {
+  return AppBar(
+    title: Text(
+      "Messaging App",
+      style: TextStyle(fontSize: 20.0, color: Colors.black),
+    ),
+  );
+}
